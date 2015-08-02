dbf43a39d5ec89ea41cf8ec4183034a7c4a149f6
Phu Nguyen <mrphu3074>
2015-08-02 11:24:39 +0700

    Upgraded MUI version to 0.10.2
    Upgraded MUI version to 0.10.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'izzilab:material-ui',
-    version: '0.1.0',
+    version: '0.1.1',
     // Brief, one-line summary of the package.
     summary: 'Material-UI using official React package',
     // URL to the Git repository containing the source code for this package.
@@ -10,7 +10,7 @@ Package.describe({
     documentation: 'README.md'
 });
 
-var MUI_VERSION = '0.10.1';
+var MUI_VERSION = '0.10.2';
 var EXTERNALIFY_VERSION = "0.1.0";
 var TAP_EVENT_PLUGIN_VERSION = '0.1.7';
 Npm.depends({
